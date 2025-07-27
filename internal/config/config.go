89cd8b3c10e3a96ddc0a01ac4c5881db960dd8ee
Agostino <agoimbimboparra@gmail.com>
2025-07-27 16:43:06 -0400

    Refactored code to have more logical file structure. Only combined files, didnt modify any acutal code.

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,77 @@
 package config
 
+import(
+	"os"
+	"fmt"
+	"encoding/json"
+)
+
 type Config struct {
 	DbURL  string `json:"db_url"`
 	CurrentUserName string `json:"current_user_name"`
+}
+
+func Read() (Config, error) {
+	configFile, err := getConfigFilePath()
+	if err != nil {
+		return Config{}, err
+	}
+	
+	data, err := os.ReadFile(configFile)
+	if err != nil {
+		return Config{}, err
+	}
+
+	var result Config
+	if err := json.Unmarshal(data, &result); err != nil {
+		return Config{}, err
+	}
+
+	return result, nil
+}
+
+
+func getConfigFilePath() (string, error) {
+	userHome, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+
+	file_path := fmt.Sprintf("%v/.gatorconfig.json", userHome)
+
+	return file_path, nil
+}
+
+func SetUser(name string) error  {
+	c, err := Read()
+	if err != nil {
+		return err
+	}
+
+	c.CurrentUserName = name
+
+	if err := write(c); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func write(cfg Config) error {
+	jsonData, err := json.Marshal(cfg)
+	if err != nil {
+		return err
+	}
+	
+	file, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(file, jsonData, 0644)
+	if err != nil {
+		return err
+	}
+	
+	return nil
 }
